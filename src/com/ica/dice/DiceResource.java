bba4b216fe9534ed65043189d9d408aa462f502d
Jeff Capuano <jcapuano328@gmail.com>
2014-07-16 07:20:59 -0400

    dice module?

---
+++ src/com/ica/dice/DiceResource.java
@@ -0,0 +1,142 @@
+package com.ica.dice;
+
+public class DiceResource {
+
+	public DiceResource() {
+	}
+		
+    // too slow?
+	/*
+	public static int getDieResource(int die, String basename) {
+        String name = "drawable/" + basename + "_" + Integer.toString(die);
+        return getResources().getIdentifier(name, null, getPackageName());
+	}
+	*/
+    
+	public static int getWhiteBlackDieResource(int die) {
+		switch(die) {
+			case 1: 
+				return R.drawable.die6_whiteblack_1;
+			case 2: 
+				return R.drawable.die6_whiteblack_2;
+			case 3: 
+				return R.drawable.die6_whiteblack_3;
+			case 4: 
+				return R.drawable.die6_whiteblack_4;
+			case 5: 
+				return R.drawable.die6_whiteblack_5;
+			case 6: 
+				return R.drawable.die6_whiteblack_6;
+		}
+		return R.drawable.die6_whiteblack_1;
+	}
+
+    public static int getRedWhiteDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_redwhite_1;
+            case 2:
+                return R.drawable.die6_redwhite_2;
+            case 3:
+                return R.drawable.die6_redwhite_3;
+            case 4:
+                return R.drawable.die6_redwhite_4;
+            case 5:
+                return R.drawable.die6_redwhite_5;
+            case 6:
+                return R.drawable.die6_redwhite_6;
+        }
+        return R.drawable.die6_redwhite_1;
+    }
+
+    public static int getBlackWhiteDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_blackwhite_1;
+            case 2:
+                return R.drawable.die6_blackwhite_2;
+            case 3:
+                return R.drawable.die6_blackwhite_3;
+            case 4:
+                return R.drawable.die6_blackwhite_4;
+            case 5:
+                return R.drawable.die6_blackwhite_5;
+            case 6:
+                return R.drawable.die6_blackwhite_6;
+        }
+        return R.drawable.die6_blackwhite_1;
+    }
+
+    public static int getBlackRedDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_blackred_1;
+            case 2:
+                return R.drawable.die6_blackred_2;
+            case 3:
+                return R.drawable.die6_blackred_3;
+            case 4:
+                return R.drawable.die6_blackred_4;
+            case 5:
+                return R.drawable.die6_blackred_5;
+            case 6:
+                return R.drawable.die6_blackred_6;
+        }
+        return R.drawable.die6_blackred_1;
+    }
+    
+    public static int getBlueWhiteDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_bluewhite_1;
+            case 2:
+                return R.drawable.die6_bluewhite_2;
+            case 3:
+                return R.drawable.die6_bluewhite_3;
+            case 4:
+                return R.drawable.die6_bluewhite_4;
+            case 5:
+                return R.drawable.die6_bluewhite_5;
+            case 6:
+                return R.drawable.die6_bluewhite_6;
+        }
+        return R.drawable.die6_bluewhite_1;
+    }
+
+    public static int getGreenWhiteDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_greenwhite_1;
+            case 2:
+                return R.drawable.die6_greenwhite_2;
+            case 3:
+                return R.drawable.die6_greenwhite_3;
+            case 4:
+                return R.drawable.die6_greenwhite_4;
+            case 5:
+                return R.drawable.die6_greenwhite_5;
+            case 6:
+                return R.drawable.die6_greenwhite_6;
+        }
+        return R.drawable.die6_greenwhite_1;
+    }
+
+    public static int getYellowBlackDieResource(int die) {
+        switch(die) {
+            case 1:
+                return R.drawable.die6_yellowblack_1;
+            case 2:
+                return R.drawable.die6_yellowblack_2;
+            case 3:
+                return R.drawable.die6_yellowblack_3;
+            case 4:
+                return R.drawable.die6_yellowblack_4;
+            case 5:
+                return R.drawable.die6_yellowblack_5;
+            case 6:
+                return R.drawable.die6_yellowblack_6;
+        }
+        return R.drawable.die6_yellowblack_1;
+    }
+    
+}
